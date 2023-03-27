6688cb7a56381aae870c74c278cc0a782965d9d2
Levente Botos <66298417+LeventeBotos@users.noreply.github.com>
2023-03-27 18:43:37 +0200

    beta

---
+++ src/pages/Ciszterciek.tsx
@@ -0,0 +1,22 @@
+const Ciszterciek = () => {
+  return (
+    <div className='cisz text-white flex flex-col justify-evenly'>
+  
+        <h1 className="font-bold p-2">Ciszterci rend</h1>
+            <div className="flex justify-evenly flex-col md:flex-row">
+                <img alt="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Medalla_San_Benito.PNG/250px-Medalla_San_Benito.PNG" />
+                <p className="self-center">A bencés rend, vagyis a Szent Benedek rendje, egy katolikus szerzetesrend, amelyet Szent Benedek alapított Itáliában a 6. században. A bencés rend a világ egyik legrégebbi és legnagyobb szerzetesrendje, amely a szerzetesi életforma szigorú követése és az oktatásban, az írásban, valamint az evangélizációban nyújtott kimagasló teljesítménye révén vívta ki hírnevét.</p>
+            </div>
+            <div className="flex justify-evenly flex-col md:flex-row">
+                <p className="self-center">A bencés szerzetesek a közösségi életforma alapján élnek, amelynek részei a szigorú napi rend, a rendszeres ima és a munka. Bár az egyes kolostorok önállóak, szoros kapcsolatban állnak egymással és az anyakolostorral.</p>
+                <img alt="oltozet" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Habit_%28Order_of_Saint_Benedict%29.svg/200px-Habit_%28Order_of_Saint_Benedict%29.svg.png" />
+            </div>
+            <div className="flex justify-evenly flex-col md:flex-row">
+                <img alt="kep" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Milkau_Das_Kloster_von_Cluny_-_Eingang_zur_Abtei_233-2.jpg/200px-Milkau_Das_Kloster_von_Cluny_-_Eingang_zur_Abtei_233-2.jpg" />
+                <p className="self-center">A bencés szerzetesek híresek a világi műveltség terjesztésére irányuló tevékenységükről, az ősi írások másolásáról és megőrzéséről, az oktatásról és a misszióról. A bencés szerzetesek kolostorai szerte a világban megtalálhatók, és sok helyen fontos szerepet játszanak az oktatásban, a kulturális életben és a közösségi szolgáltatásokban.</p>
+            </div>
+      </div>
+  )
+}
+
+export default Ciszterciek
