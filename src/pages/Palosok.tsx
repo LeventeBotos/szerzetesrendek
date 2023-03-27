3642b7da179a0e325c6b41e2c9de807340bb23df
Levente Botos <66298417+LeventeBotos@users.noreply.github.com>
2023-03-27 22:10:15 +0200

    done3

--- src/pages/Palosok.tsx
+++ src/pages/Palosok.tsx
@@ -5,19 +5,25 @@ const Palosok = () => {
     <h1 className="font-bold p-5 m-5 text-6xl lg:text-9xl">Pálos rend</h1>
         <div className="flex justify-evenly flex-col md:flex-row">
             <img alt="logo" src="https://upload.wikimedia.org/wikipedia/commons/8/81/The_Coat_of_Arms_of_the_Pauline_Fathers.png" />
+            <div className="flex flex-col justify-evenly">
             <p className="self-center">A Pálos rendet magyarok alapítja Boldog Özséb a közpkorban.</p>
+            <p>Remete Szent Pál a névadója.</p>
+
+            </div>
+
         </div>
-        <div className=""><p>Remete Szent Pál a névadója.</p></div>
+
         <div className="flex justify-evenly flex-col md:flex-row">
             <p className="self-center">Eredeti székhelye a Budakeszi út és Szépjuhászné körül volt, de mikor II. József király feloszlatta a rendet, átkerült a központja Częstochowa-ba.</p>
             <img alt="hely" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Poland_adm_location_map.svg/248px-Poland_adm_location_map.svg.png" />
         </div>
         <div className="flex justify-evenly flex-col md:flex-row">
             <img alt="kep" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Rockchapel.JPG/230px-Rockchapel.JPG" />
-            <p className="self-center">Mostani legfőbb temploma a Szikla kápolna</p>
+            <p className="self-center">A címerükön Remete Szent Pálhoz kapcsolódó dolgok szerepelnek, mint a pálmafa, ami táplálta Pált, az oroszlán, ami Pál sírját ásta ki, és a holló egy cipővel a csőrében, ami a remeteségét fejezi ki.</p>
+
         </div>
-        
-        <div className="p-4"><p>A címerükön Remete Szent Pálhoz kapcsolódó dolgok szerepelnek, mint a pálmafa, ami táplálta Pált, az oroszlán, ami Pál sírját ásta ki, és a holló egy cipővel a csőrében, ami a remeteségét fejezi ki.</p></div>
+        <p className="self-center">Mostani legfőbb temploma a Szikla kápolna</p>
+        <div className="p-4"></div>
         Készitették:
       <ul className="font-bold">
         <li>Botos Levente</li>
