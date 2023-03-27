9aea59a57a29517d2a0b95a572df21a2431ece3c
Levente Botos <66298417+LeventeBotos@users.noreply.github.com>
2023-03-27 22:08:00 +0200

    done2

---
+++ src/pages/Ferencesek.tsx
@@ -0,0 +1,37 @@
+const Ferencesek = () => {
+  return (
+    <div className='fer text-white flex flex-col justify-evenly'>
+  
+        <h1 className="font-bold p-5 m-5 text-6xl lg:text-9xl">Ferences rend</h1>
+            <div className="flex justify-evenly flex-col md:flex-row">
+                <img alt="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Coat_of_Arms_of_the_Order_of_Friars_Minor.svg/375px-Coat_of_Arms_of_the_Order_of_Friars_Minor.svg.png" />
+                <div className="flex flex-col justify-evenly">
+                    <p className="">Assisi Szent Ferenc alapította 1209-ben.</p>
+                    <p className="">Gazdag családból származott, katona volt, mikor megsérül a háborúban, hazakerül és lázálmai lesznek betegsége alatt. </p>
+                    <p className="">Szembesül a földi javal valódi értéktelenségével.</p>
+                    <p>Mindent vissza ad apjának, és kivonul a városon kívülre, az evangélium alapján akar élni.</p>
+                </div>    
+            </div>
+            <div className="flex justify-evenly flex-col md:flex-row">
+                <div className="flex flex-col justify-evenly">
+                <p className="self-center">Hála és öröm lelkiség jellemzi, ezt hívjuk ferences lelkiségnek.</p>
+                <p>Isten közelségét keresi a természetben, testvéri viszony alakul ki a tagok között, mindenkit el kell fogadni, mert mindenki Isten ajándéka.</p>
+                <p>Isten felé alázatot mutatnak. Egy kolduló rend, azaz szegénységben élnek, szinte nincstelenül.</p>
+                </div>
+                
+                <img alt="oltozet" src="https://www.magyarkurir.hu/img1.php?id=64448&v=0&img=o_assisiferenc.jpg" />
+            </div>
+            <div className="flex justify-evenly flex-col md:flex-row">
+                <img alt="kep" src="https://www.ferencesek.hu/wp-content/themes/ferencesek/images/rendhazak/Esztergomi_Ferences_Rendhaz.jpg" />
+                <p className="self-center">Magyarországon az Esztergomi Ferences rendházban vannak legfőképpen jelen, de akár Budapesten is találunk ferences templomokat, például a Ferenciek terén. </p>
+            </div>
+      </div>
+  )
+}
+
+export default Ferencesek
+
+{/* <p>A ferences rend, vagyis a Szent Ferenc rendje, egy katolikus szerzetesrend, amelyet Szent Ferenc alapított Olaszországban a 13. században. A rend tagjai a szerzetesi fogadalmat teszik, és a szegénységre, az egyszerű életre, a lemondásra és az evangélium életvitel szerinti követésére esküsznek.</p>
+    <p>Az eredeti célja az volt, hogy a szerzetesek az evangéliumot a világ szegényeinek hirdessék, és szolgálják őket. A ferences rend tagjai általában a papi szolgálat mellett tanítanak, az evangéliumot prédikálják, kórházakat és szociális szolgáltatásokat működtetnek, valamint más módon segítenek a szegényeken és a rászorulókon.</p>
+    <p>A ferences rend számos szervezettel rendelkezik, amelyek közé tartozik a ferences harmadrend, amelynek tagjai a világiak, akik ugyanazokat az elveket és életformákat követik, mint a rendi szerzetesek.</p>
+    <p>A ferences rend a katolikus egyház egyik legnagyobb szerzetesrendje, tagjai szerte a világban megtalálhatók. A rendet különösen jellemzi a szegénységi fogadalom, amely szerint a tagok elkötelezik magukat a szegénység és az egyszerű életvitel mellett.</p> */}
--- src/pages/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <div className="bg-white text-black font-bold">
-
-    </div>
-  )
-}
-
-export default Footer
